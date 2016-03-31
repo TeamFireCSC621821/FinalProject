713d6e3fc7d89f53e076142b70b63b7710b6e372
brianxautumn <shred@brianautumn.com>
2016-03-31 04:55:52 -0700

    Got next step but image won't display

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
+
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageToVTKImageFilter.h"
+#include "vtkImageViewer.h"
+#include "vtkRenderWindowInteractor.h"
+
 #include <iostream>
+
 
 using namespace std;
 
-int main() {
-    cout << "Hello, test" << endl;
+int main(int argc, char **argv) {
+    typedef itk::Image<unsigned short,2> ImageType;
+    typedef itk::ImageFileReader<ImageType> ReaderType;
+    typedef itk::ImageToVTKImageFilter < ImageType> ConnectorType;
+    ReaderType::Pointer reader = ReaderType::New();
+    ConnectorType::Pointer connector = ConnectorType::New();
+
+    reader->SetFileName( "~/mic.jpg" );
+    connector->SetInput( reader->GetOutput() );
+
+    vtkImageViewer * viewer = vtkImageViewer::New();
+    vtkRenderWindowInteractor * renderWindowInteractor = vtkRenderWindowInteractor::New();
+
+    viewer->SetupInteractor( renderWindowInteractor );
+    viewer->SetInputData(connector->GetOutput());
+    viewer->Render();
+    viewer->SetColorWindow( 20 );
+    viewer->SetColorLevel( 0 );
+    renderWindowInteractor->Start();
 
 
     return 0;
