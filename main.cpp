18ccef95bf011d2cfab05bc134b1f17df91bb9fc
brianxautumn <shred@brianautumn.com>
2016-04-29 00:28:47 -0700

    Got 3d to work but need better rendering method

--- main.cpp
+++ main.cpp
@@ -4,7 +4,21 @@
 #include "itkImageToVTKImageFilter.h"
 #include "vtkImageViewer.h"
 #include "vtkRenderWindowInteractor.h"
+#include "itkImageSeriesReader.h"
 #include "itkCurvatureFlowImageFilter.h"
+#include <vtkDICOMImageReader.h>
+#include "itkGDCMSeriesFileNames.h"
+#include "itkGDCMImageIO.h"
+#include <vtkImageSliceMapper.h>
+#include <vtkImageMapper.h>
+#include <vtkImageSlice.h>
+#include <vtkVolumeProperty.h>
+#include <vtkSmartVolumeMapper.h>
+#include <vtkSmartPointer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkImageViewer2.h>
 
 //For Edge Preserving Smoothing
 #include "itkGradientAnisotropicDiffusionImageFilter.h"
@@ -17,29 +31,67 @@
 
 using namespace std;
 
-int main(int argc, char **argv) {
-    //typedef itk::Image<unsigned short,2> ImageType;
-    //typedef itk::Image< float , 2 > ImageType;
-    //typedef itk::CurvatureFlowImageFilter<ImageType, ImageType> SmoothingFilterType;
+// helper class to format slice status message
+class StatusMessage {
+public:
+    static std::string Format(int slice, int maxSlice) {
+        std::stringstream tmp;
+        tmp << "Slice Number  " << slice + 1 << "/" << maxSlice + 1;
+        return tmp.str();
+    }
+};
 
+int main(int argc, char **argv) {
+
+    const unsigned int      Dimension = 3;
+    typedef float InputPixelType;
+    typedef float  OutputPixelType;
+    typedef itk::Image< InputPixelType, Dimension > InputImageType;
+    typedef itk::Image< OutputPixelType, Dimension > OutputImageType;
+
+    //typedef itk::Image< InputPixelType, Dimension > ImageType;
+    typedef itk::ImageSeriesReader< InputImageType >        ReaderType;
+    typedef itk::GDCMImageIO       ImageIOType;
+    ImageIOType::Pointer dicomIO = ImageIOType::New();
+
+    ReaderType::Pointer reader = ReaderType::New();
+
+    reader->SetImageIO( dicomIO );
+    typedef itk::GDCMSeriesFileNames NamesGeneratorType;
+    NamesGeneratorType::Pointer nameGenerator = NamesGeneratorType::New();
+
+    //nameGenerator->SetUseSeriesDetails( true );
+    //nameGenerator->AddSeriesRestriction("0008|0021" );
+    nameGenerator->SetDirectory( "/Users/mac/DICOMSLIDES");
+
+/*
     typedef float InputPixelType;
     typedef float OutputPixelType;
-    typedef itk::Image< InputPixelType, 2 > InputImageType;
-    typedef itk::Image< OutputPixelType, 2 > OutputImageType;
-
-
-    typedef itk::GradientAnisotropicDiffusionImageFilter<InputImageType, OutputImageType > FilterType;
-    FilterType::Pointer filter = FilterType::New();
+    typedef itk::Image< InputPixelType, Dimension > InputImageType;
+    typedef itk::Image< OutputPixelType, Dimension > OutputImageType;
+*/
 
 
 
+    typedef std::vector< std::string >    SeriesIdContainer;
+    const SeriesIdContainer & seriesUID = nameGenerator->GetSeriesUIDs();
+    std::cout << seriesUID.size() << std::endl;
+    SeriesIdContainer::const_iterator seriesItr = seriesUID.begin();
+    SeriesIdContainer::const_iterator seriesEnd = seriesUID.end();
+    while( seriesItr != seriesEnd )
+    {
+        std::cout << seriesItr->c_str() << std::endl;
+        seriesItr++;
+    }
+    std::string seriesIdentifier;
+    seriesIdentifier = seriesUID.begin()->c_str();
+    std::cout << seriesIdentifier.c_str() << std::endl;
 
-    typedef itk::ImageFileReader<InputImageType> ReaderType;
-    typedef itk::ImageToVTKImageFilter < OutputImageType > ConnectorType;
-    ReaderType::Pointer reader = ReaderType::New();
-    ConnectorType::Pointer connector = ConnectorType::New();
+    typedef std::vector< std::string >   FileNamesContainer;
+    FileNamesContainer fileNames;
+    fileNames = nameGenerator->GetFileNames( seriesIdentifier );
 
-    reader->SetFileName(argv[1] );
+    reader->SetFileNames( fileNames );
     try
     {
         reader->Update();
@@ -49,29 +101,103 @@ int main(int argc, char **argv) {
         std::cout << ex << std::endl;
     }
 
-    filter->SetInput( reader->GetOutput() );
-    filter->SetNumberOfIterations( 5 );
-    filter->SetTimeStep( 0.125 );
-    filter->SetConductanceParameter( 3.0 );
-    filter->Update();
 
+    typedef itk::GradientAnisotropicDiffusionImageFilter<InputImageType,InputImageType > FilterType;
+    FilterType::Pointer filter = FilterType::New();
+
+    filter->SetInput( reader->GetOutput() );
+    filter->SetNumberOfIterations( 1 );
+    filter->SetTimeStep( 0.01 );
+    filter->SetConductanceParameter( 3.0 );
+
+
+
+    try
+    {
+        filter->Update();
+    }
+    catch( itk::ExceptionObject & error )
+    {
+        std::cerr << "Error: " << error << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    typedef itk::ImageToVTKImageFilter < InputImageType > ConnectorType;
+    ConnectorType::Pointer connector = ConnectorType::New();
 
     connector->SetInput( filter->GetOutput() );
     connector->Update();
 
-    vtkImageViewer * viewer = vtkImageViewer::New();
-    vtkRenderWindowInteractor * renderWindowInteractor = vtkRenderWindowInteractor::New();
-
-    viewer->SetupInteractor( renderWindowInteractor );
-    viewer->SetInputData(connector->GetOutput());
 
 
+    /**
+     * VTK
+     *
+     */
+
+    vtkSmartPointer<vtkImageSliceMapper> imageSliceMapper = vtkSmartPointer<vtkImageSliceMapper>::New();
+    //imageSliceMapper->SetInputConnection(importer->GetOutputPort());
+    imageSliceMapper->SetInputData(connector->GetOutput());
+
+    //imageSliceMapper->SetInputData(importer->GetOutput());
+    imageSliceMapper->Update();
 
 
-    viewer->Render();
-    viewer->SetColorWindow( 255 );
-    viewer->SetColorLevel( 128 );
+
+/*
+    vtkSmartPointer<vtkImageSlice> imageSlice = vtkSmartPointer<vtkImageSlice>::New();
+    imageSlice->SetMapper(imageSliceMapper);
+*/
+
+
+    vtkSmartPointer<vtkSmartVolumeMapper> volumeMapper = vtkSmartPointer<vtkSmartVolumeMapper>::New();
+    volumeMapper->SetBlendModeToComposite();
+    //volumeMapper->SetInputConnection(importer->GetOutputPort());
+    volumeMapper->SetInputData(connector->GetOutput());
+
+    vtkSmartPointer<vtkVolumeProperty> volumeProperty = vtkSmartPointer<vtkVolumeProperty>::New();
+    volumeProperty->ShadeOff();
+    volumeProperty->SetInterpolationType(VTK_LINEAR_INTERPOLATION);
+
+
+
+    // Setup renderers
+    vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+    //renderer->AddViewProp(imageSlice);
+    renderer->ResetCamera();
+    renderer->SetBackground(0.1,0.4,0.2);
+
+    // Setup render window
+    vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+    renderWindow->SetSize(600, 600);
+    renderWindow->AddRenderer(renderer);
+
+
+
+    vtkSmartPointer<vtkVolume> volume = vtkSmartPointer<vtkVolume>::New();
+    volume->SetMapper(volumeMapper);
+    volume->SetProperty(volumeProperty);
+    renderer->AddViewProp(volume);
+
+
+
+    renderer->ResetCamera();
+    volumeMapper->SetRequestedRenderModeToRayCast();
+    volumeMapper->SetBlendModeToMinimumIntensity();
+
+    renderWindow->Render();
+
+    vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+            vtkSmartPointer<vtkRenderWindowInteractor>::New();
+
+    // Render and start interaction
+    renderWindowInteractor->SetRenderWindow(renderWindow);
+    renderWindowInteractor->Initialize();
+
     renderWindowInteractor->Start();
+
+
+
 
 
     return 0;
